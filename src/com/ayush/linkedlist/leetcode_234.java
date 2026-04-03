813e75404d8aa5ff3d07f3e1f17f999c87840525
ayush81z <ayushkumar811op@gmail.com>
2026-04-03 23:59:49 +0530

    optimise

--- src/com/ayush/linkedlist/leetcode_234.java
+++ src/com/ayush/linkedlist/leetcode_234.java
@@ -16,18 +16,15 @@ public class leetcode_234 {
       }
     }
 
-
-
         public boolean isPalindrome(ListNode head) {
-            if (  head == null || head.next == null ) {
+            if (head == null) {
                 return true;
             }
             ListNode mid = mid(head);
-            ListNode revsec = reverse(mid);
-            ListNode rerev = reverse(revsec);
+            ListNode second = reverse(mid.next);
+            mid.next = null; //fr the split
 
             ListNode first = head;
-            ListNode second = revsec;
 
             while(second != null) {
                 if (first.val != second.val) {
@@ -48,28 +45,19 @@ public class leetcode_234 {
                 current.next = prev;
                 prev = current;
                 current = next;
-                if (next != null) {
-                    next = next.next;
-                }
             }
             return prev;
         }
 
         public ListNode mid(ListNode head) {
-        if ( head == null) {
-            return head;
-        }
         ListNode slow = head;
         ListNode fast = head;
 
-        while ( fast != null && fast.next != null && fast.next.next != null ) {
+        while (fast.next != null && fast.next.next != null ) {
             slow = slow.next;
             fast = fast.next.next;
         }
-        if ( fast .next == null) {
-            return slow;
-        }
-        return slow.next;
+        return slow;
         }
 
 
